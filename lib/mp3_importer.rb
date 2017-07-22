7797ed0e53cf1b6b5b0446a57a14f324c3e1dcfd
linkcoding <kenny.lin.91@gmail.com>
2017-07-21 23:33:10 -0400

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,8 +6,22 @@ class MP3Importer
   end
 
   def files
-    true_path = "#{path}.*mp3"
-    files = Dir[true_path].each {|file| require file }
+    true_path = "#{@path}/*.mp3"
+    collection = Dir[true_path]
+    collection.map do |song|
+      song.split("/").last
+    end
     #binding.pry
+  end
+
+  def import
+    songs = files.map do |file|
+      file.split(" - ")
+    end
+    #binding.pry
+    songs.each do |song|
+      Artist.find_or_create_by_name(song[0]).add_song(song[1])
+      #binding.pry
+    end
   end
 end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(file)
+    split_file = file.split(" - ")
+    song = self.new(split_file[1])
+    song.artist = Artist.find_or_create_by_name(split_file[0])
+    song
+  end
+end
